70694960bfa7aa2da511a0b191cfb96a8b1d6829
MuhammadIhsan45 <muhsanz41@gmail.com>
2022-12-02 21:22:55 +0700

    commite 1

---
+++ src/main/java/pertemuan100/pertemuan100/Pertemuan100Application.java
@@ -0,0 +1,13 @@
+package pertemuan100.pertemuan100;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pertemuan100Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pertemuan100Application.class, args);
+	}
+
+}
---
+++ src/test/java/pertemuan100/pertemuan100/Pertemuan100ApplicationTests.java
@@ -0,0 +1,13 @@
+package pertemuan100.pertemuan100;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pertemuan100ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
